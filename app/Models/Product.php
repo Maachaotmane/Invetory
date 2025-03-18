ce9a1477e821f50d2d6f240b219612426f55c7e3
kamalghrabla <kamalghrabla@gmail.com>
2025-03-18 02:58:01 +0100

    models

---
+++ app/Models/Product.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id', 'fournisseur_id', 'unit_id', 'brand_id', 'type_id',
+        'measure_id', 'sub_category_id', 'store_id', 'reference', 'name', 'description'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function fournisseur()
+    {
+        return $this->belongsTo(Fournisseur::class);
+    }
+
+    public function unit()
+    {
+        return $this->belongsTo(Unit::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function measure()
+    {
+        return $this->belongsTo(Measure::class);
+    }
+
+    public function subCategory()
+    {
+        return $this->belongsTo(SubCategory::class);
+    }
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function variants()
+    {
+        return $this->hasMany(ProductVariant::class);
+    }
+}
---
+++ app/Models/ProductVariant.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductVariant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id', 'variant', 'quantity', 'quantity_alert', 'price', 'buying_price'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
